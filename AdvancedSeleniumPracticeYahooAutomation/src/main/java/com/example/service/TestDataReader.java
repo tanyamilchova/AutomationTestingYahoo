c04c0d13a248a2305a7c865014482b66797633db
Tanya Milchova <margot11margot11@gmail.com>
2025-02-14 11:39:24 +0200

    TestDataReader modification

--- AdvancedSeleniumPracticeYahooAutomation/src/main/java/com/example/service/TestDataReader.java
+++ AdvancedSeleniumPracticeYahooAutomation/src/main/java/com/example/service/TestDataReader.java
@@ -10,51 +10,33 @@ import java.util.ResourceBundle;
 public class TestDataReader {
     private static final Logger logger = LoggerFactory.getLogger(TestDataReader.class);
 
-//    public static ResourceBundle resourceBundle ;
-//
-//    private static final String ENVIRONMENT = System.getenv("ENVIRONMENT") != null ?
-//            System.getenv("ENVIRONMENT") :
-//            System.getProperty("environment", "qa");
-//
-//    private static ResourceBundle getResourceBundle() {
-//        if (resourceBundle == null) {
-//            resourceBundle = ResourceBundle.getBundle(ENVIRONMENT);
-//        }
-//        return resourceBundle;
-//    }
-//    public static String getTestData(String key) {
-//        if (key == null || key.isBlank()) {
-//            logger.warn("Attempted to retrieve a blank or null property key.");
-//            throw new IllegalArgumentException("The property is null or empty.");
-//        }
-//
-//        String envValue = System.getenv(key);
-//        if (envValue != null && !envValue.trim().isBlank()) {
-//            return envValue;
-//        }
-//        try {
-//            return getResourceBundle().getString(key);
-//        } catch (Exception exception) {
-//           logger.error("Failed to retrieve property key: {}", key, exception);
-//            throw new NotFoundException("Key: " + key + " does not exist in properties");
-//        }
-//    }
-static {
-//    System.setProperty("environment", "qa");
-    System.setProperty("environment", "config");
-}
-    public static ResourceBundle resourceBundle=ResourceBundle.getBundle(System.getProperty("environment"));
-    public static String getTestData(String key){
-        if(key != null){
-            String envValue = System.getenv(key);
-            if (envValue != null && !envValue.trim().isEmpty()) {
-                return envValue;
-            }
+    public static ResourceBundle resourceBundle ;
+
+    private static final String ENVIRONMENT = System.getenv("ENVIRONMENT") != null ?
+            System.getenv("ENVIRONMENT") :
+            System.getProperty("environment", "qa");
+
+    private static ResourceBundle getResourceBundle() {
+        if (resourceBundle == null) {
+            resourceBundle = ResourceBundle.getBundle(ENVIRONMENT);
+        }
+        return resourceBundle;
+    }
+    public static String getTestData(String key) {
+        if (key == null || key.isBlank()) {
+            logger.warn("Attempted to retrieve a blank or null property key.");
+            throw new IllegalArgumentException("The property is null or empty.");
+        }
+
+        String envValue = System.getenv(key);
+        if (envValue != null && !envValue.trim().isBlank()) {
+            return envValue;
         }
         try {
-            return resourceBundle.getString(key);
-        } catch (Exception e) {
-            return null;
+            return getResourceBundle().getString(key);
+        } catch (Exception exception) {
+           logger.error("Failed to retrieve property key: {}", key, exception);
+            throw new NotFoundException("Key: " + key + " does not exist in properties");
         }
     }
 }
